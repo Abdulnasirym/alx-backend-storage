04a69fd05d4a22ff7a326ec0f1df1c908fd200e5
abdulnasirym <abdulnasiry05@gmail.com>
2024-10-22 16:24:37 -0700

    listdocument in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,4 +5,4 @@ def list_all(mongo_collection):
     """list all document in a collection"""
     if mongo_collection == None:
         return []
-    return List(mongo_collection.find())
+    return list(mongo_collection.find())
